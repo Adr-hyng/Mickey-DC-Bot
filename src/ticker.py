0efdfdb9132f5cb8892fcf464e1faea15db344d0
Replit user <>
2022-11-26 12:39:23 +0000

    update: fix seconds

--- src/ticker.py
+++ src/ticker.py
@@ -1,4 +1,4 @@
-from credentials import Channel, DiscordBot, Credentials # Ticker --> Credentials
+from credentials import Channel, DiscordBot, Credentials  # Ticker --> Credentials
 from discord import Status
 from discord.ext import tasks
 
@@ -7,13 +7,20 @@ tree = DiscordBot.tree.value
 channel = Channel
 credentials = Credentials
 
-@tasks.loop(seconds=5)
+
+@tasks.loop(seconds=30)
 async def guild_tick():
-    total_members = len(client.users)
-    online_members = len([member for member in client.get_all_members() if member.status == Status.online])
-    await client.get_channel(channel.STATISTICS.value).edit(name = f"📊STATISTICS: {total_members}")
-    await client.get_channel(channel.ONLINE.value).edit(name = f"Online: {online_members}")
-    
+	total_members = len(client.users)
+	online_members = len([
+	 member for member in client.get_all_members()
+	 if member.status == Status.online
+	])
+	await client.get_channel(channel.STATISTICS.value
+	                         ).edit(name=f"📊STATISTICS: {total_members}")
+	await client.get_channel(channel.ONLINE.value
+	                         ).edit(name=f"Online: {online_members}")
+
+
 @guild_tick.before_loop
 async def before():
-    await client.wait_until_ready()
+	await client.wait_until_ready()
